f02e0a56e3a62293a1e250f431e8c7850188568e
Sukhjit Singh <sukhjitsehra@gmail.com>
2023-02-18 16:45:01 -0500

    Initialized the repo with directory structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This repo contains the instuructions for a machine learning project.',
+    author='Sukhjit Singh Sehra',
+    license='',
+)
